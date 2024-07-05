9674be9a34afe250a476a4cf5731ceeebb7b7915
Felix Günther <felix.gnthr@gmail.com>
2024-07-05 09:54:10 +0200

    better titel

--- R/mod_top_detections.R
+++ R/mod_top_detections.R
@@ -13,7 +13,7 @@ mod_top_detections_ui <- function(id) {
   ns <- NS(id)
   tagList(
     tags$script(src = "www/audio_player.js"),
-    h3("Höre Rufe und Gesänge"),
+    h3("Rufe und Gesänge anhören"),
     htmlOutput(ns("species_cards"))
   )
 }
