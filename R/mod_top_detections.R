a82ee0e917f9913e38c816bf00652308c6ffa54f
Felix Günther <felix.gnthr@gmail.com>
2024-07-05 08:31:02 +0200

    Lieder -> Gesänge

--- R/mod_top_detections.R
+++ R/mod_top_detections.R
@@ -13,7 +13,7 @@ mod_top_detections_ui <- function(id) {
   ns <- NS(id)
   tagList(
     tags$script(src = "www/audio_player.js"),
-    h3("Höre Rufe und Lieder"),
+    h3("Höre Rufe und Gesänge"),
     htmlOutput(ns("species_cards"))
   )
 }
