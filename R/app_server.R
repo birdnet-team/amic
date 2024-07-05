826f16767be7563f02ef4362b891015f5365b512
Felix Günther <felix.gnthr@gmail.com>
2024-07-05 09:53:53 +0200

    set minconfidence to 0.85

--- R/app_server.R
+++ R/app_server.R
@@ -9,6 +9,7 @@
 app_server <- function(input, output, session) {
   #
   project <- "pam_in_chemnitz"
+  min_confidence <- 0.85
 
   # used to invalidate the cache every 10 minutes
   rounded_time <- reactive({
@@ -25,14 +26,22 @@ app_server <- function(input, output, session) {
 
   # Get and cache Data
   recorder_species_count_all <- reactive({
-    ecopiapi::get_recorderspeciescounts(project_name = project, start_date = "2020-01-01") |>
+    ecopiapi::get_recorderspeciescounts(
+      project_name = project,
+      start_date = "2020-01-01",
+      min_confidence = min_confidence
+      ) |>
       left_join(birdnames)
   }) |>
     bindCache(rounded_time())
 
 
   recorder_species_count_today <- reactive({
-    ecopiapi::get_recorderspeciescounts(project_name = project) |>
+    ecopiapi::get_recorderspeciescounts(
+      project_name = project,
+      min_confidence = min_confidence
+      # start_date by default set to today
+      ) |>
       left_join(birdnames)
   }) |>
     bindCache(rounded_time())
@@ -41,13 +50,14 @@ app_server <- function(input, output, session) {
     ecopiapi::get_detections(
       order_by = "-datetime",
       limit = "none",
-      # confidence__gte = 0.1,
+      confidence__gte = min_confidence,
       datetime__gte = Sys.Date() |> paste0("T00:00:00Z"),
       only = c("species_code", "datetime"),
       project_name = project
     ) |>
       left_join(birdnames)
-  })
+  }) |>
+    bindCache(rounded_time())
 
   # Use nested modules for value boxes
   mod_value_box_server("box_today", recorder_species_count_today, "Alle Erkennungen Heute")
